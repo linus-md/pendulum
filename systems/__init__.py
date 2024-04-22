cd5d352ae90da6c8860c4d58e549b93719e042f0
linus-md <linus.sommer@protonmail.com>
2024-04-22 20:26:25 +0200

    Add double pendulum with only one parameter

--- systems/__init__.py
+++ systems/__init__.py
@@ -1,9 +1,13 @@
 from .pendulum.single import single
+
 from .pendulum.double_simple import double_simple
+from .pendulum.double_half import double_half
 from .pendulum.double_full import double_full
 from .pendulum.double_full_g import double_full_g
+
 from .pendulum.triple_simple import triple_simple
 from .pendulum.triple_full import triple_full
 from .pendulum.triple_full_g import triple_full_g
+
 from .kepler.kepler_simple import kepler_simple
 from .kepler.kepler_full import kepler_full
