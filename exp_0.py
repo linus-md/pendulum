5a3026f606c573b87241d8d1a0fa2c0826ede771
linus-md <linus.sommer@protonmail.com>
2024-04-23 15:08:26 +0200

    run benchmark

--- exp_0.py
+++ exp_0.py
@@ -1,6 +1,6 @@
-from core.main import algorithm
+from core.main import algorithm_0
 from systems.benchmark.double_ll import double_ll
 
 qi, pi = double_ll()
-result = algorithm(qi, pi)
+result = algorithm_0(qi, pi)
 print(result)
