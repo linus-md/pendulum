cf9ea1a902c0087e467af8d9b1b88f16b7b2e8e7
linus-md <linus.sommer@protonmail.com>
2024-04-24 11:36:51 +0200

    fix

--- exp_1.py
+++ exp_1.py
@@ -2,5 +2,5 @@ from core.main import algorithm
 from systems.benchmark.double_ll import double_ll
 
 qi, pi = double_ll()
-result = algorithm(qi, pi)
+result = algorithm(qi, pi, method='singular:std')
 print(result)
