4da9b1229724a1304c87f8c4e7c588aaab077016
linus-md <linus.sommer@protonmail.com>
2024-04-25 20:40:15 +0200

    ? fix

--- systems/benchmark/chem_fake.py
+++ systems/benchmark/chem_fake.py
@@ -5,18 +5,21 @@ from sage.all import PolynomialRing, QQ
 def chem_fake():
     R = PolynomialRing(QQ, 'x1, x2, x3, x4, x5, x6, k1, k2, k3, k4, k5, k6', 
                        order='degrevlex')
-    pi = [R('- k1*x1 + k4*x3*x5'),    
-          R('  k1*x1 - k2*x2 + k5*x4*x5'),
-          R('- k3*x3 + k2*x2 - k4*x3*x5'), # used to be  ....
-          R('  k3*x3 - k5*x4*x5'),
-          R('- k4*x3*x5 - k5*x4*x5 + k6*x6'),
-          R('  k4*x3*x5 - k6*x6 + k5*x4*x5'),
-          R('0'),
-          R('0'),
-          R('0'),
-          R('0'),
-          R('0'),
-          R('0')]
+    
+    pi = [
+        R('- k1*x1 + k4*x3*x5'), 
+        R(' k1*x1 - k2*x2 + k5*x4*x5'),
+        R('- k3*x3 + k2*x2 + k4*x3*x5'),
+        R(' k3*x3 - k5*x4*x5'),
+        R('- k4*x3*x5 - k5*x4*x5 + k6*x6'),
+        R(' k4*x3*x5 - k6*x6 + k5*x4*x5'),
+        R('0'),
+        R('0'),
+        R('0'),
+        R('0'),
+        R('0'),
+        R('0')
+    ]
 
     qi = [R('x1 + x2 + x3 - x4 - 1'), R('x5 + x6 - 1')]
     return qi, pi
