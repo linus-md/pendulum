bc20510ec41a90185836af5af5c0102c48eb9ca4
linus-md <linus.sommer@protonmail.com>
2024-04-25 11:26:23 +0200

    comment

--- systems/benchmark/chem_erk.py
+++
@@ -1,3 +0,0 @@
-from sage.all import PolynomialRing, QQ
-
-# See 3.1 in Oscillations and bistability in a model of ERK regulation
--- systems/hard/chem_reduced_erk.py
+++ systems/hard/chem_reduced_erk.py
@@ -1,6 +1,7 @@
 from sage.all import PolynomialRing, QQ
 
 # From Torres et al. (2019) 3.3
+# Needs to be checked
 
 def reduced_erk():
     vars = 'x1, x2, x3, x4, x5, x6, x7, x8, x9, x10,' + \
