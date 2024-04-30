200bc0f30a715930361a51b3abb089e2cc27741f
linus-md <linus.sommer@protonmail.com>
2024-04-30 14:42:54 +0200

    experiments

--- core/main.py
+++ core/main.py
@@ -13,23 +13,23 @@ def diff_op(qi, pi):
 
 def algorithm(qi, pi):
     i = 0
-    logger.info('Algorithm 1 started')
-    logger.info(f'Iteration {i} - {datetime.datetime.now()}')
+   #logger.info('Algorithm 1 started')
+   #logger.info(f'Iteration {i} - {datetime.datetime.now()}')
     G = ideal(qi).groebner_basis()
-    # logger.info(G)
-    logger.info('')
+    ##logger.info(G)
+   #logger.info('')
 
     while True:
         qi = [diff_op(qs, pi) for qs in qi]
         qi = [qs.reduce(G) for qs in qi]
-        logger.info(f'Monomial order: {qi[0].parent().term_order()}')
+       #logger.info(f'Monomial order: {qi[0].parent().term_order()}')
 
         if any(qs != 0 for qs in qi):
             i += 1
-            logger.info(f'Iteration {i} - {datetime.datetime.now()}')
+           #logger.info(f'Iteration {i} - {datetime.datetime.now()}')
             G = ideal(list(set(G + qi))).groebner_basis()
-            print(G)
-            # logger.info(G)
+            #print(G)
+            ##logger.info(G)
         else:
             return G
 
@@ -48,22 +48,49 @@ def algorithm_0(qi, pi):
         if any(qs != 0 for qs in qi):
             i += 1
             I = I + ideal(qi)
+            # print the number of gens
+            #logger.info(f'Number of generators: {len(I.gens())}')
+
             #logger.info(f'')
             #logger.info(f'Iteration {i} - {datetime.datetime.now()}')
-            logger.info(f'Monomial order: {qi[0].parent().term_order()}')
-            print(I.groebner_basis())
-            # logger.info(I)
+            #logger.info(f'Monomial order: {qi[0].parent().term_order()}')
+            #print(I.groebner_basis())
+            ##logger.info(I)
         else:
             # This gets computed eventhough we compute it for the reduction above...
             return ideal(I).groebner_basis()
 
+
+def algorithm_exp(qi, pi):
+    i = 0
+    #logger.info('Algorithm 1 started')
+    #logger.info(f'Iteration {i} - {datetime.datetime.now()}')
+    S = qi 
+    G = ideal(qi).groebner_basis()
+    ##logger.info(G)
+    #logger.info('')
+
+    while True:
+        qi = [diff_op(qs, pi) for qs in qi]
+        qi = [qs.reduce(G) for qs in qi]
+        #logger.info(f'Monomial order: {qi[0].parent().term_order()}')
+
+        if any(qs != 0 for qs in qi):
+            i += 1
+            #logger.info(f'Iteration {i} - {datetime.datetime.now()}')
+            S = list(set(S + qi))
+            G = ideal(S).groebner_basis()
+            # print(G)
+            ##logger.info(G)
+        else:
+            return G
+
 if __name__ == '__main__':
     from systems import chem_fake, double, single
     from sage.all import PolynomialRing, QQ
+    import timeit
     
-    time = datetime.datetime.now()
-    qi, pi = double()
-    res = algorithm_0(qi, pi)
-    
-    print(res)
-    print(f"{(datetime.datetime.now() - time).total_seconds()}s")
+    n = 1
+    qi, pi = chem_fake()
+    execution_time = timeit.timeit(lambda: algorithm_exp(qi, pi), number=n)
+    print(execution_time/n)
--- maple/algorithms.mpl
+++ maple/algorithms.mpl
@@ -10,10 +10,10 @@ algorithm := proc(qs, p, vars)
 	i := 0; 
 	tord := tdeg(op(vars)); 
 	gs := qs; 
+    print(gs);
 	G := Basis(qs, tord); 
 	while true do
-		i := i + 1; 
-		print(cat("Iteration ", i, " g= ", g)); 
+		i := i + 1;
 		gs := map(g ->  NormalForm(partial(g, p, vars), G, tord), gs);
 		gs := remove(`=`, gs, 0);
 		if nops(gs) = 0 then
@@ -21,6 +21,38 @@ algorithm := proc(qs, p, vars)
 		fi;
 		G := [op(G), op(gs)]; 
 		G := Basis(G, tord); 
-		print(G); end 	do; 
+		end do; 
 	return G; 
 end proc;
+
+# Simple double pendulum
+vars := [x1, y1, u1, v1, x2, y2, u2, v2];
+p := [
+    u1, 
+    v1, 
+    - x1 - (x1 - x2), 
+    - y1 - (y1 - y2) - 1, 
+    u2, 
+    v2, 
+    x2 - x1, 
+    y2 - y1 - 1
+    ];
+q := [x1^2 + y1^2 - 1, (x2 - x1)^2 + (y2 - y1)^2 - 1];
+
+#algorithm(q, p, vars);
+
+
+# Not really a Chemical reaction network (394s)
+vars1 := [x1, x2, x3, x4, x5, x6, k1, k2, k3, k4, k5, k6];
+p1 := [
+    - k1*x1 + k4*x3*x5, 
+    k1*x1 - k2*x2 + k5*x4*x5, 
+    - k3*x3 + k2*x2 + k4*x3*x5, 
+    k3*x3 - k5*x4*x5,
+    - k4*x3*x5 - k5*x4*x5 + k6*x6, 
+    k4*x3*x5 - k6*x6 + k5*x4*x5,
+    0, 0, 0, 0, 0, 0
+    ];
+q1 := [x1 + x2 + x3 + x4 - 1, x5 + x6 - 1];
+
+algorithm(q1, p1, vars1);
